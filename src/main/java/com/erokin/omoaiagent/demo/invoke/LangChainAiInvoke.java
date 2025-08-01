5958aea0b26fea8c5a78082beb49d9769b2c10a2
EROQIN <1107891366@qq.com>
2025-08-01 21:26:25 +0800

    feat(demo): 添加 AI调用示例
    - 在 application.yml 中添加本地环境配置
    - 新增 LangChainAiInvoke 类，演示如何使用 LangChain 调用 AI
    - 新增 SpringAiAiInvoke 类，展示如何使用 Spring AI 框架调用 AI
    - 在 pom.xml 中添加相关依赖

---
+++ src/main/java/com/erokin/omoaiagent/demo/invoke/LangChainAiInvoke.java
@@ -0,0 +1,14 @@
+package com.erokin.omoaiagent.demo.invoke;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+
+public class LangChainAiInvoke {
+    public static void main(String[] args) {
+        QwenChatModel qwenChatModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-plus")
+                .build();
+        String result = qwenChatModel.chat("你是谁喵？");
+        System.out.println(result);
+    }
+}
