b3c72a90ccac9ce8b1a53a5fae5c043b7e76767c
EROQIN <1107891366@qq.com>
2025-08-02 22:39:13 +0800

    feat(chatMemory): 实现基于文件的对话记忆持久化
    - 新增 FileBasedChatMemory 类，实现 ChatMemory 接口
    - 使用 Kryo 序列化库进行对象持久化
    - 修改 LoveApp，支持使用文件基于的对话记忆
    - 更新 .gitignore，忽略 tmp 目录
    - 在 pom.xml 中添加 kryo 依赖

--- src/main/java/com/erokin/omoaiagent/app/LoveApp.java
+++ src/main/java/com/erokin/omoaiagent/app/LoveApp.java
@@ -1,11 +1,10 @@
 package com.erokin.omoaiagent.app;
 
 import com.erokin.omoaiagent.advisor.MySimpleLoggerAdvisor;
-import com.erokin.omoaiagent.advisor.ReReadingAdvisor;
+import com.erokin.omoaiagent.chatMemory.FileBasedChatMemory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
-import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
@@ -20,13 +19,17 @@ import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvis
 @Component
 @Slf4j
 public class LoveApp {
+
     private final ChatClient chatClient;
 
     private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。\n";
-
     public LoveApp(ChatModel dashScopeChatModel){
         //初始化基于内存的对话记忆
-        ChatMemory chatMemory = new InMemoryChatMemory();
+        // ChatMemory chatMemory = new InMemoryChatMemory();
+        //基于文件的对话记忆
+        String fileDir = System.getProperty("user.dir") + "/tmp/chat-memory";
+        ChatMemory chatMemory = new FileBasedChatMemory(fileDir);
+
         chatClient = ChatClient
                 .builder(dashScopeChatModel)
                 .defaultSystem(SYSTEM_PROMPT)
@@ -39,6 +42,7 @@ public class LoveApp {
                 )
                 .build();
     }
+
 
     /**
      * AI 基础对话（支持多轮对话记忆）
