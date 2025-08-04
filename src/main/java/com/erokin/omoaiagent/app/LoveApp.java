e3b9e57a3d6dd502728ce7ed08d2ae2849ba09b2
EROQIN <1107891366@qq.com>
2025-08-05 00:08:29 +0800

    feat(rag): 整合 PGVector 向量存储
    - 在 application.yml 中添加了 AI 向量存储的配置
    - 在 LoveApp 中注入了 PgVectorVectorStore
    - 新增 PgVectorVectorStoreConfig 配置类
    - 添加了 PgVectorVectorStore 的测试用例
    - 更新了 pom.xml，添加了相关依赖

--- src/main/java/com/erokin/omoaiagent/app/LoveApp.java
+++ src/main/java/com/erokin/omoaiagent/app/LoveApp.java
@@ -32,7 +32,13 @@ public class LoveApp {
     @Resource
     private Advisor loveAppRagCloudAdvisor;
 
+    @Resource
+    private VectorStore pgVectorVectorStore;
+
+
+
     private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。\n";
+
     public LoveApp(ChatModel dashScopeChatModel){
         //初始化基于内存的对话记忆
         // ChatMemory chatMemory = new InMemoryChatMemory();
@@ -106,6 +112,8 @@ public class LoveApp {
                 // .advisors(new QuestionAnswerAdvisor(loveAppVectorStore))
                 // 应用 RAG 检索增强服务（基于云知识库）
                 .advisors(loveAppRagCloudAdvisor)
+                // 应用 RAG 检索增强服务（基于PGVector向量存储）
+                // .advisors(new QuestionAnswerAdvisor(pgVectorVectorStore))
                 .call()
                 .chatResponse();
         String content = chatResponse.getResult().getOutput().getText();
