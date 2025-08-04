aed509dabd74fc632855509a7b29445f86fb9f82
EROQIN <1107891366@qq.com>
2025-08-04 18:29:24 +0800

    feat(rag): 添加恋爱大师 RAG 功能
    - 新增 RAG 相关配置和 advisor- 添加向量库和文档加载器
    - 实现基于 RAG 的聊天功能
    - 增加相关测试用例

--- src/main/java/com/erokin/omoaiagent/app/LoveApp.java
+++ src/main/java/com/erokin/omoaiagent/app/LoveApp.java
@@ -2,13 +2,17 @@ package com.erokin.omoaiagent.app;
 
 import com.erokin.omoaiagent.advisor.MySimpleLoggerAdvisor;
 import com.erokin.omoaiagent.chatMemory.FileBasedChatMemory;
+import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
 import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -21,6 +25,12 @@ import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvis
 public class LoveApp {
 
     private final ChatClient chatClient;
+
+    @Resource
+    private VectorStore loveAppVectorStore;
+
+    @Resource
+    private Advisor loveAppRagCloudAdvisor;
 
     private static final String SYSTEM_PROMPT = "扮演深耕恋爱心理领域的专家。开场向用户表明身份，告知用户可倾诉恋爱难题。围绕单身、恋爱、已婚三种状态提问：单身状态询问社交圈拓展及追求心仪对象的困扰；恋爱状态询问沟通、习惯差异引发的矛盾；已婚状态询问家庭责任与亲属关系处理的问题。引导用户详述事情经过、对方反应及自身想法，以便给出专属解决方案。\n";
     public LoveApp(ChatModel dashScopeChatModel){
@@ -82,5 +92,28 @@ public class LoveApp {
         // log.info("loveReport:{}", loveReport);
         return loveReport;
     }
+
+
+    public String doChatWithRag(String message, String chatId) {
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                // 开启日志，便于观察效果
+                .advisors(new MySimpleLoggerAdvisor())
+                // 应用知识库问答
+                // .advisors(new QuestionAnswerAdvisor(loveAppVectorStore))
+                // 应用 RAG 检索增强服务（基于云知识库）
+                .advisors(loveAppRagCloudAdvisor)
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
+
+
 
 }
