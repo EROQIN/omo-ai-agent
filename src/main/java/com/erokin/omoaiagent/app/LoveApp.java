bdc1f08d77088001e8ecb4c7d8ca6be3e6fb6a95
EROQIN <1107891366@qq.com>
2025-08-10 00:50:38 +0800

    feat(app): 集成 MCP 功能
    - 在 application.yml 中添加 MCP 客户端配置- 在 LoveApp 中实现使用 MCP 进行对话的方法
    - 添加 mcp-servers.json 配置文件
    - 在 pom.xml 中添加 spring-ai-mcp-client 依赖

--- src/main/java/com/erokin/omoaiagent/app/LoveApp.java
+++ src/main/java/com/erokin/omoaiagent/app/LoveApp.java
@@ -15,6 +15,7 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Component;
 
@@ -157,6 +158,31 @@ public class LoveApp {
         log.info("content: {}", content);
         return content;
     }
+    @Resource
+    private ToolCallbackProvider toolCallbackProvider;
+
+    /**
+     * 使用MCP进行对话
+     * @param message
+     * @param chatId
+     * @return
+     */
+    public String doChatWithMcp(String message, String chatId) {
+        ChatResponse response = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec -> spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 10))
+                // 开启日志，便于观察效果
+                .advisors(new MySimpleLoggerAdvisor())
+                .tools(toolCallbackProvider)
+                .call()
+                .chatResponse();
+        String content = response.getResult().getOutput().getText();
+        log.info("content: {}", content);
+        return content;
+    }
+
 
 
 
